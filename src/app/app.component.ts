d9b9abad214d930f8406d95ba916b444ca329c51
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-06-25 02:40:50 -0400

    feat: remove confirmation dialog component from app root imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ConfirmationDialogComponent } from './core/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConfirmationDialogComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
