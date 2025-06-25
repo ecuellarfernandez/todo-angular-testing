83ca52e33483a579db4a4914c33f93e76f1de1f5
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-06-25 00:28:55 -0400

    refactor: enhance login UI with improved layout and error handling

--- src/app/auth/presentation/login/login.component.ts
+++ src/app/auth/presentation/login/login.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 import {AuthRepository} from '../../domain/repositories/auth.repository';
 import {AuthRepositoryImpl} from '../../data/auth-repository.impl';
 import {LoginUseCase} from '../../domain/usecases/login.usecase';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: 'login.component.html',
   providers:[
@@ -20,7 +22,13 @@ import {LoginUseCase} from '../../domain/usecases/login.usecase';
 })
 export class LoginComponent implements OnInit {
   form: any;
-  constructor(private fb: FormBuilder, private loginUseCase: LoginUseCase) {}
+  errorMessage: string = '';
+  
+  constructor(
+    private fb: FormBuilder, 
+    private loginUseCase: LoginUseCase,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -34,10 +42,23 @@ export class LoginComponent implements OnInit {
       const email = this.form.get('email')?.value!;
       const password = this.form.get('password')?.value!;
       this.loginUseCase.execute(email, password).subscribe({
-        next: (user) => console.log('Usuario autenticado:', user),
-        error: (err) => console.error('Error en la autenticación:', err),
+        next: (user) => {
+          console.log('Usuario autenticado:', user);
+          localStorage.setItem('jwt', user.token);
+          this.router.navigate(['/projects']);
+        },
+        error: (err) => {
+          console.error('Error en la autenticación:', err);
+          this.errorMessage = 'Credenciales inválidas. Por favor, inténtelo de nuevo.';
+        },
       });
+    } else {
+      this.form.markAllAsTouched();
     }
+  }
+  
+  navigateToRegister() {
+    this.router.navigate(['/register']);
   }
 
 }
