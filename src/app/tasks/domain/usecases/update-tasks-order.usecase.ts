58242ffefefd523ab5a5b325e56af9cd3a387070
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-07-09 15:28:47 -0400

    feat: implement drag and drop functionality for task reordering in dashboard

---
+++ src/app/tasks/domain/usecases/update-tasks-order.usecase.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../models/task.model';
+import { TaskRepository } from '../repositories/task.repository';
+
+@Injectable()
+export class UpdateTasksOrderUseCase {
+  constructor(private taskRepository: TaskRepository) {}
+
+  execute(projectId: string, todoListId: string, taskIds: string[]): Observable<Task[]> {
+    return this.taskRepository.updateTasksOrder(projectId, todoListId, taskIds);
+  }
+}
