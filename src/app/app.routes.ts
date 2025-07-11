e08c7bb1d57840bc6e75e507a96239c2d2b0f95e
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-07-11 12:40:52 -0400

    feat: add Cypress commands for API interactions and ToDoList creation tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/presentation/login/login.component';
 import { RegisterComponent } from './auth/presentation/register/register.component';
-import { ProjectListComponent } from './projects/presentation/project-list/project-list.component';
-import { TodoListListComponent } from './todolists/presentation/todolist-list/todolist-list.component';
-import { TaskListComponent } from './tasks/presentation/task-list/task-list.component';
 import { authGuard } from './core/guards/auth.guard';
 
 import { DashboardComponent } from './core/components/dashboard/dashboard.component';
@@ -26,23 +23,6 @@ export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
-  },
-  {
-    path: 'projects',
-    component: ProjectListComponent,
-    canActivate: [authGuard]
-  },
-
-  {
-    path: 'projects/:id/todolists',
-    component: TodoListListComponent,
-    canActivate: [authGuard]
-  },
-
-  {
-    path: 'projects/:projectId/todolists/:todoListId/tasks',
-    component: TaskListComponent,
-    canActivate: [authGuard]
   },
 
 ];
